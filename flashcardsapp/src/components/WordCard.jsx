b6ec253b55544a479120bf7a44f948289f337e41
darina bebenina <darinabebenina@yahoo.com>
2023-06-30 02:08:50 +0500

    useState at wordcard added

--- flashcardsapp/src/components/WordCard.jsx
+++ flashcardsapp/src/components/WordCard.jsx
@@ -1,13 +1,49 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './WordCard.scss';
 
 function WordCard(props) {
-    return (
-        <div className="wordCard-container">
-            <div className="wordcard">
-                <div className="word"><h1>props.english</h1></div>
+    const [translationMode, setTranslationMode] = useState(false);
+
+    const handleTranslation = () => {
+        setTranslationMode(!translationMode);
+    };
+
+    const handleEnglish = () => {
+        setTranslationMode(!translationMode);
+    };
+
+    const english = () => {
+        return (
+            <div className="wordCard-container" onClick={handleTranslation}>
+                <div className="wordcard">
+                    <div className="word__english">
+                        <h1>props.english</h1>
+                        <div className="word__transcription">
+                            <h2>props.transcription</h2>
+                        </div>
+                    </div>
+                </div>
             </div>
-        </div>
+        )
+    }
+
+    const translation = () => {
+        return (
+            <div className="wordCard-container" onClick={handleTranslation}>
+                <div className="wordcard">
+                    <div className="word__english">
+                        <h1>props.russian</h1>
+                    </div>
+                </div>
+            </div>
+        )
+    }
+
+
+    return (
+        <>
+            {translationMode ? translation() : english()}
+        </>
     )
 }
 
